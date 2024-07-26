da5376695e57e94dd4bd43f7a209d532ef210aba
Lloyd Allen <lloydallen597@gmail.com>
2024-07-26 00:20:30 -0500

    Matched the Animation to Movement

--- RPG Core Project/Assets/Scripts/Mover.cs
+++ RPG Core Project/Assets/Scripts/Mover.cs
@@ -1,13 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-
-    
-
     // Update is called once per frame
     void Update()
     {
@@ -15,7 +13,7 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
-        
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -27,5 +25,13 @@ public class Mover : MonoBehaviour
         {
             GetComponent<UnityEngine.AI.NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
