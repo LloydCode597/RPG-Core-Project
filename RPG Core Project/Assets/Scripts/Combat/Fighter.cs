6ec93e4f354495997aed3a4ffa668b4f87394b94
Lloyd Allen <lloydallen597@gmail.com>
2024-08-01 21:39:17 -0500

    Added Free Animation Pack
    Implemented Attack Animation and Trigger Animation In Code.

--- RPG Core Project/Assets/Scripts/Combat/Fighter.cs
+++ RPG Core Project/Assets/Scripts/Combat/Fighter.cs
@@ -23,7 +23,13 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
+        }
+
+        private void AttackBehaviour()
+        {
+            GetComponent<Animator>().SetTrigger("attack");
         }
 
         private bool GetIsInRange()
@@ -39,6 +45,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        // Animation Event
+        void Hit()
+        {
+
         }
     }
 }
