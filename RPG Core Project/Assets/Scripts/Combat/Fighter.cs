02c7e84ebf66f7d58384070904d5867365036d7e
Lloyd Allen <lloydallen597@gmail.com>
2024-08-10 23:17:48 -0500

    Fixed a Few Bugs

--- RPG Core Project/Assets/Scripts/Combat/Fighter.cs
+++ RPG Core Project/Assets/Scripts/Combat/Fighter.cs
@@ -38,14 +38,21 @@ namespace RPG.Combat
             if (timeSinceLastAttack > timeBetweenAttacks)
             {
                 // This will trigger the Hit() event. 
-                GetComponent<Animator>().SetTrigger("attack");
+                TriggerAttack();
                 timeSinceLastAttack = 0;
             }
         }
 
+        private void TriggerAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("stopAttack");
+            GetComponent<Animator>().SetTrigger("attack");
+        }
+
+        // Animation Event
         void Hit()
         {
-            if (target == null) return; //Blocking mechanism to avoid null reference.
+            if (target == null) { return; } //Blocking mechanism to avoid null reference.
             target.TakeDamage(weaponDamage);
         }
 
@@ -68,9 +75,14 @@ namespace RPG.Combat
         }
         public void Cancel()
         {
-            GetComponent<Animator>().SetTrigger("stopAttack");
-            GetComponent<Animator>().ResetTrigger("attack");
+            StopAttack();
             target = null;
+        }
+
+        private void StopAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("attack");
+            GetComponent<Animator>().SetTrigger("stopAttack");
         }
     }
 }
