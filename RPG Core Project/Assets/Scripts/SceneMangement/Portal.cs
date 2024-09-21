7f0d36e07b31e1a1a64309cdd412404df03638a0
Lloyd Allen <lloydallen597@gmail.com>
2024-09-21 01:31:39 -0500

    Avoiding The Singleton Pattern

--- RPG Core Project/Assets/Scripts/SceneMangement/Portal.cs
+++ RPG Core Project/Assets/Scripts/SceneMangement/Portal.cs
@@ -15,6 +15,9 @@ namespace RPG.SceneManagement
         [SerializeField] int sceneToLoad = -1;
         [SerializeField] Transform spawnPoint;
         [SerializeField] DestinationIdentifier destination;
+        [SerializeField] float fadeOutTime = 1f;
+        [SerializeField] float fadeInTime = 2f;
+        [SerializeField] float fadeWaitTime = 0.5f;
         private void OnTriggerEnter(Collider other)
         {
             print("Entering Portal");
@@ -32,6 +35,9 @@ namespace RPG.SceneManagement
                 yield break;
             }
             DontDestroyOnLoad(gameObject);
+            Fader fader = FindObjectOfType<Fader>();
+
+            yield return fader.FadeOut(fadeOutTime);
             AsyncOperation loadingScene = SceneManager.LoadSceneAsync(sceneToLoad);
             while (!loadingScene.isDone)
             {
@@ -39,6 +45,8 @@ namespace RPG.SceneManagement
             }
             Portal otherPortal = GetOtherPortal();
             UpdatePlayer(otherPortal);
+            yield return new WaitForSeconds(fadeWaitTime);
+            yield return fader.FadeIn(fadeInTime);
             Destroy(gameObject);
         }
 
