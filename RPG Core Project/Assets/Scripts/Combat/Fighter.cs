86384f4758f56aad01ea457176c52b4661d9357d
Lloyd Allen <lloydallen597@gmail.com>
2024-11-04 23:57:28 -0600

    Working on Equipping a Weapon to the Player on Instantiate

--- RPG Core Project/Assets/Scripts/Combat/Fighter.cs
+++ RPG Core Project/Assets/Scripts/Combat/Fighter.cs
@@ -11,8 +11,15 @@ namespace RPG.Combat
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponDamage = 5f;
+        [SerializeField] GameObject weaponPrefab = null;
+        [SerializeField] Transform handTransform = null;
         Health target;
         float timeSinceLastAttack = Mathf.Infinity;
+
+        private void Start()
+        {
+            SpawnWeapon();
+        }
 
         private void Update()
         {
@@ -31,7 +38,13 @@ namespace RPG.Combat
                 AttackBehaviour();
             }
         }
-
+        private void SpawnWeapon()
+        {
+            if (weaponPrefab != null)
+            {
+                Instantiate(weaponPrefab, handTransform);
+            }
+        }
         private void AttackBehaviour()
         {
             transform.LookAt(target.transform);
